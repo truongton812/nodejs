18c3d66c08e13945917b58100a42ddb4111e2452
Tuna Le <truongton812@gmail.com>
2022-07-15 00:18:57 +0700

    add new branch

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb://localhost:27017", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
